667b9aa4e94bc1318feaad971e2e9f31d4fa7b14
wuriyanto <wuriyanto48@yahoo.co.id>
2019-05-01 18:41:41 +0700

    initial commit

---
+++ website2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/welcome", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	http.ListenAndServe(":8001", nil)
+}
